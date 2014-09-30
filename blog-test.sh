8f02aa7299509a6d68f58f401672b7f2e0dafce7
lalyos <lalyos@yahoo.com>
2014-09-30 06:41:14 +0200

    blog-test script fixed browser opening timing

--- blog-test.sh
+++ blog-test.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-
-: ${BLOG_PORT:=8080}
 cat <<EOF
-blog is beeing generated
-a browser will automatically
-open up after 30sec serving:
 
 ============================
- http://192.168.59.103:${BLOG_PORT}
+blog generation started ...
+be pateinet it takes about 20 sec
+red warnings are ok
+if finished browser will open:
+
+  http://192.168.59.103:${BLOG_PORT}
 ============================
 
 EOF
 
+: ${BLOG_PORT:=8080}
 export BLOG_PORT
-(sleep 30; open http://192.168.59.103:${BLOG_PORT}) &
 
-docker build -t delme . && docker run -p ${BLOG_PORT}:8080 delme
+# stop previous blog contaier
+docker rm -f  blog-test &> /dev/null
+
+docker build -t blog-test-image . \
+ && docker run -d --name blog-test -p ${BLOG_PORT}:8080 blog-test-image \
+ && open http://192.168.59.103:${BLOG_PORT}
+
+cat <<EOF
+
+========================================================
+to clean up the container, and the image:
+
+  docker rm -f blog-test && docker rmi blog-test-image
+========================================================
