859d798f6ed4b05aa7be2393cf6e1a6714f8dc97
lalyos <lalyos@yahoo.com>
2014-09-30 07:38:23 +0200

    dirty option added to blog-test script

--- blog-test.sh
+++ blog-test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cat <<EOF
-
 ============================
 blog generation started ...
 be pateinet it takes about 20 sec
@@ -12,8 +11,16 @@ if finished browser will open:
 
 EOF
 
+: ${DIRTY:=false}
 : ${BLOG_PORT:=8080}
 export BLOG_PORT
+
+if [[ $1 =~ -d ]]; then
+  echo '[WARNING] you are testing uncommited changes !'
+else
+  echo [INFO] stashing away uncommited changes and untracked files
+  git stash save -u "before blog generation"
+fi
 
 # stop previous blog contaier
 docker rm -f  blog-test &> /dev/null
@@ -21,6 +28,13 @@ docker rm -f  blog-test &> /dev/null
 docker build -t blog-test-image . \
  && docker run -d --name blog-test -p ${BLOG_PORT}:8080 blog-test-image \
  && open http://192.168.59.103:${BLOG_PORT}
+
+if [[ $1 =~ -d ]]; then
+  echo '[WARNING] Please remember to commit you changes'
+else
+  echo [INFO] Move uncommited changes and untracked files back to WORKDIR
+  git stash pop
+fi
 
 cat <<EOF
 
